117cc43c251ae562c02edc5d39b2b20366c8e556
aurelien.thomas <aurelien.thomas@etu.univ-tlse2.fr>
2018-03-28 14:31:45 +0200

    Init Server

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# LeagueOfStones
-Game based on Web technologies merging Hearthstone rules with League of Legends characters
+## Start Server 
+
+```
+$ npm start
+```
+
+Server starts on localhost:8080
+
+## API 
+
+### GLOBAL
+
+GET / 
+
+GET /users/getAll
+
+GET /cards/getAll 
+
+### USERS
+
+GET /users/subscribe 
+
+GET /users/unsubscribe
+
+GET /users/connect
+
+GET /users/disconnect
+
+GEt /users/amIConnected
+
+### MATCH
+
+GET /match/getMatch
+
+GET /match/getAllMatch
+
+GET /match/initDeck
+
+GET /match/pickCard
+
+GET /match/playCard
+
+GET /match/attack
+
+GET /match/attackPlayer
+
+GET /match/endTurn
+
+GET /match/finishMatch
+
+### MATCHMAKING
+
+GET /matchmaking/participate
+
+GET /matchmaking/unparticipate
+
+GET /matchmaking/getAll
+
+GET /matchmaking/request
+
+GEt /matchmaking/acceptRequest
---
+++ app.js
@@ -0,0 +1,75 @@
+var fs = require('fs');
+var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var sess = require('express-session');
+var cors = require('cors');
+
+
+var tools = require('./modules/tools.js');
+var users = require('./modules/users.js');
+var matchmaking = require('./modules/matchmaking.js');
+var match = require('./modules/match.js');
+var cards = require('./modules/cards.js');
+
+var app = express();
+
+var losDB = null;
+function initApp(losDB) {
+  //Allowing CORS connection
+  app.all("*", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Content-Type', 'application/json');
+    console.log("REQUEST : " + req.originalUrl);
+    if (req.query)
+      console.log(req.query);
+    if (req.query.token) {
+      losDB.sessionStore.get(req.query.token, function (error, session) {
+        if (error === null) {
+          req.session = session;
+        }
+        next();
+      });
+    }
+    else
+      next();
+  });
+
+  //init session config
+  losDB.sessionStore = new sess.MemoryStore();
+  app.use(sess({
+    secret: 'Les loutres ça poutre!',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 7200000 }, // keep session activate during 2hours,
+    store: losDB.sessionStore
+  }));
+  app.use(cors());
+
+  app.get('/', function (req, res) {
+    tools.sendData(res, "Hello World ! ");
+  });
+
+  users.init(app, tools, losDB);
+  matchmaking.init(app, tools, losDB);
+  cards.init(app, tools, losDB);
+  match.init(app, tools, losDB, cards);
+
+  var serverPort = 8080;
+  app.listen(serverPort, function () {
+    console.log('server up and running at %s port', serverPort);
+  });
+}
+
+
+
+
+MongoClient.connect('mongodb://localhost:27017', function (err, client) {
+  if (err) {
+    throw err;
+  }
+  losDB = client.db('League_Of_Stones');
+  console.log("MONGO DB initialised : ");
+  console.log(losDB.s.databaseName);
+  initApp(losDB); //init app only when mongodb connection is set up
+});
